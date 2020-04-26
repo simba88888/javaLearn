8603a31e6afca1d7ae39a8f95c7dcb094de84aa2
yangbin <ybinybsh@cn.ibm.com>
2020-04-26 16:33:21 +0800

    Format codes

--- src/demo_base/singlePattern/DoubleCheckPattern.java
+++ src/demo_base/singlePattern/DoubleCheckPattern.java
@@ -2,9 +2,11 @@ package demo_base.singlePattern;
 
 public class DoubleCheckPattern {
     private static volatile DoubleCheckPattern single;
-    private DoubleCheckPattern(){
+
+    private DoubleCheckPattern() {
 
     }
+
     public static DoubleCheckPattern getInstance() {
         if (single == null) {
             synchronized (DoubleCheckPattern.class) {
