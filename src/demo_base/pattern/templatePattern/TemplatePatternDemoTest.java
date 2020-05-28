d52f7b2684e26b7066e986e154dd12d53d33ddd7
yangbin <ybinybsh@cn.ibm.com>
2020-05-28 19:40:39 +0800

    Add template pattern

---
+++ src/demo_base/pattern/templatePattern/TemplatePatternDemoTest.java
@@ -0,0 +1,69 @@
+package demo_base.pattern.templatePattern;
+
+abstract class BaseSteps {
+    public final void templateMethod() {
+        handleFirst();
+        handleSecond();
+        handleThirdth();
+        handleFourth();
+    }
+
+    private void handleFirst() {
+        System.out.println(BaseSteps.class.getSimpleName() + " handleFirst");
+    }
+
+    protected abstract void handleSecond();
+
+    protected abstract void handleThirdth();
+
+    protected abstract void handleFourth();
+}
+
+class ConcreA extends BaseSteps {
+
+    @Override
+    protected void handleSecond() {
+        System.out.println(ConcreA.class.getSimpleName() + " " + "handleSecond");
+    }
+
+    @Override
+    protected void handleThirdth() {
+        System.out.println(ConcreA.class.getSimpleName() + " " + "handleThirdth");
+    }
+
+    @Override
+    protected void handleFourth() {
+        System.out.println(ConcreA.class.getSimpleName() + " " + "handleFourth");
+    }
+}
+
+class ConcreB extends BaseSteps {
+
+    @Override
+    protected void handleSecond() {
+        System.out.println(ConcreB.class.getSimpleName() + " " + "handleSecond");
+    }
+
+    @Override
+    protected void handleThirdth() {
+        System.out.println(ConcreB.class.getSimpleName() + " " + "handleThirdth");
+    }
+
+    @Override
+    protected void handleFourth() {
+        System.out.println(ConcreB.class.getSimpleName() + " " + "handleFourth");
+    }
+}
+
+
+public class TemplatePatternDemoTest {
+    public static void main(String[] args) {
+        BaseSteps baseSteps = new ConcreA();
+        baseSteps.templateMethod();
+
+        System.out.println();
+
+        baseSteps = new ConcreB();
+        baseSteps.templateMethod();
+    }
+}
